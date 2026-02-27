7c0cb9cb623bab12c0d141892b991b55c5ed1856
nic-tolentino <hello@nictolentino.com>
2026-02-28 09:34:31 +1300

    Fix SQL ingestion query statement bug

--- backend/src/main/kotlin/com/techmarket/persistence/ingestion/IngestionQueries.kt
+++ backend/src/main/kotlin/com/techmarket/persistence/ingestion/IngestionQueries.kt
@@ -4,5 +4,5 @@ import com.techmarket.persistence.IngestionFields
 
 object IngestionQueries {
     fun getSelectAllSql(datasetName: String, ingestionsTableName: String) =
-            "SELECT ${IngestionFields.ID}, ${IngestionFields.SOURCE}, ${IngestionFields.INGESTED_AT}, ${IngestionFields.RAW_PAYLOAD} FROM $datasetName.$ingestionsTableName"
+            "SELECT ${IngestionFields.ID}, ${IngestionFields.SOURCE}, ${IngestionFields.INGESTED_AT}, ${IngestionFields.RAW_PAYLOAD} FROM `$datasetName.$ingestionsTableName`"
 }
